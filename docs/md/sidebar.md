e69a3e2cfe009e68b034d9b518d410b39ba1d3e8
MCJack123 <jackmacwindowslinux@gmail.com>
2019-10-21 00:01:32 -0400

    Started working on documentation

---
+++ docs/md/sidebar.md
@@ -0,0 +1,6 @@
+### [Main Page](index.html)
+### Introduction
+* [What is CraftOS-PC?](about.html)
+* [Downloading & Installing](installation.html)
+* [Getting Started](getting-started.html)
+* [New Features](new-features.html)
--- index.js
+++ index.js
@@ -88,17 +88,22 @@ function ready() {
     getLatest();
     showDetails();
     const icons = FaUserAgent.faUserAgent(navigator.userAgent);
-    document.getElementById("download-button").innerHTML = '<span class="download-icon">' + icons.os.html + "</span></i>&nbsp;" + document.getElementById("download-button").innerHTML;
+    document.getElementById("download-button").innerHTML = '<span class="download-icon">' + (icons.platform.name == "desktop" ? icons.os.html : '<i class="fab fa-github">') + "</i></span>&nbsp;" + document.getElementById("download-button").innerHTML;
     var link = document.getElementById("download-link");
     if (icons.platform.name == "desktop") {
         if (icons.os.name == "apple") link.href = "https://github.com/MCJack123/craftos2/releases/download/latest/CraftOS-PC.dmg";
         else if (icons.os.name == "windows") link.href = "https://github.com/MCJack123/craftos2/releases/download/latest/CraftOS-PC-Setup.exe";
-        else if (icons.os.name == "linux") link.href = "linux-instructions.html";
-        else console.log("Unknown platform " + icons.os.name);
+        else if (icons.os.name == "linux") link.href = "docs/installation.html#Linux";
+        else {
+            link.href = "https://github.com/MCJack123/craftos2/";
+            document.getElementsByClassName("download-text")[0].innerText = "View on GitHub";
+            document.getElementById("download-source").remove();
+        }
         if (icons.os.name == "apple") document.getElementById("download-platform").innerText = "macOS";
-        else document.getElementById("download-platform").innerText = icons.os.name.charAt(0).toUpperCase() + icons.os.name.slice(1);
+        else if (document.getElementById("download-platform") != undefined) document.getElementById("download-platform").innerText = icons.os.name.charAt(0).toUpperCase() + icons.os.name.slice(1);
     } else {
-        link.href = "javascript:alert('Unsupported platform.')";
-        document.getElementById("download-platform").innerText = "Unknown"
+        link.href = "https://github.com/MCJack123/craftos2/";
+        document.getElementsByClassName("download-text")[0].innerText = "View on GitHub";
+        document.getElementById("download-source").remove();
     }
 }
