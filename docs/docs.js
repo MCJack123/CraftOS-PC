20f90a649746a60570b4144503b9104645fbb4b5
MCJack123 <jackmacwindowslinux@gmail.com>
2020-05-19 23:14:53 -0400

    Added breadcrumbs, fixed docs 404

--- docs/docs.js
+++ docs/docs.js
@@ -13,7 +13,7 @@ function toggleNav() {
 document.addEventListener("DOMContentLoaded", function() {
     var dir = location.search == "" ? "md" : location.search.substr(1);
     var xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", function() {document.getElementById("docs-content").innerHTML = marked(this.responseText); if (location.hash != "") location.hash = location.hash; setTimeout(function() {if (dir != "md") for (var elem of document.getElementsByTagName("a")) elem.href += "?" + dir;}, 1);});
+    xhr.addEventListener("load", function() {if (this.status != 200) {document.getElementById("docs-content").innerHTML = "<h1>Page Not Found</h1><p>The requested document could not be found.</p>"; return;} document.getElementById("docs-content").innerHTML = marked(this.responseText); if (location.hash != "") location.hash = location.hash; setTimeout(function() {if (dir != "md") for (var elem of document.getElementsByTagName("a")) elem.href += "?" + dir;}, 1);});
     xhr.open("GET", dir + "/" + (location.href.replace(".html", "") + ".md").split("/").reverse()[0]);
     xhr.send();
     var xhr2 = new XMLHttpRequest();
