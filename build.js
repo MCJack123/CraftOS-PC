019f4026676636867cdb1d1598617ae5438e4962
MCJack123 <jackmacwindowslinux@gmail.com>
2021-03-10 11:40:20 -0500

    Added automatic image sizing
    Could this fix CLS? Also fixed a link.

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const ejs = require("ejs");
 const fetch = require("node-fetch");
 const fs = require("fs/promises");
 const highlight = require("highlight.js/lib/core");
+const imageSize = require("image-size");
 const marked = require("marked");
 const process = require("process");
 const details = require("./src/homepage-details.json");
@@ -17,31 +18,39 @@ marked.setOptions({
     }
 });
 
-fetch("https://api.github.com/repos/MCJack123/craftos2/releases/latest", {headers: {"User-Agent": "CraftOS-PC-Build/1.0 nodejs/" + process.version}})
-    .then(res => res.json())
-    .then(json => ejs.renderFile("src/index.ejs", {details: details, tag: json.tag_name}))
-    .then(str => fs.writeFile("build/index.html", str, {encoding: "utf8"}))
-    .catch(err => {
+let sizes = {}
+fs.readdir("build/images").then(imageFiles => {
+    for (let file of imageFiles) if (file !== "webp") sizes[file.replace(".png", "")] = imageSize("build/images/" + file);
+
+    fetch("https://api.github.com/repos/MCJack123/craftos2/releases/latest", {headers: {"User-Agent": "CraftOS-PC-Build/1.0 nodejs/" + process.version}})
+        .then(res => res.json())
+        .then(json => ejs.renderFile("src/index.ejs", {details: details, tag: json.tag_name, imageSizes: sizes}))
+        .then(str => fs.writeFile("build/index.html", str, {encoding: "utf8"}))
+        .catch(err => {
+            console.error(err);
+            process.exit(1);
+        });
+
+    Promise.all([
+        fs.readFile("docs/sidebar.md", {encoding: "utf8"}),
+        fs.readdir("docs")
+    ]).then(res => {
+        let sidebar = marked(res[0]);
+        for (let file of res[1]) {
+            if (file !== "sidebar.md") {
+                fs.readFile("docs/" + file, {encoding: "utf8"})
+                    .then(data => ejs.renderFile("src/docs.ejs", {info: docsInfo[file.match(/[^%.]+/)], sidebar: sidebar, content: marked(data)}))
+                    .then(data => fs.writeFile("build/docs/" + file.replace(".md", ".html"), data, {encoding: "utf8"}))
+                    .catch(err => {
+                        console.error(err);
+                        process.exit(1);
+                    });
+                }
+        }
+    }).catch(err => {
         console.error(err);
         process.exit(1);
     });
-
-Promise.all([
-    fs.readFile("docs/sidebar.md", {encoding: "utf8"}),
-    fs.readdir("docs")
-]).then(res => {
-    let sidebar = marked(res[0]);
-    for (let file of res[1]) {
-        if (file !== "sidebar.md") {
-            fs.readFile("docs/" + file, {encoding: "utf8"})
-                .then(data => ejs.renderFile("src/docs.ejs", {info: docsInfo[file.match(/[^%.]+/)], sidebar: sidebar, content: marked(data)}))
-                .then(data => fs.writeFile("build/docs/" + file.replace(".md", ".html"), data, {encoding: "utf8"}))
-                .catch(err => {
-                    console.error(err);
-                    process.exit(1);
-                });
-            }
-    }
 }).catch(err => {
     console.error(err);
     process.exit(1);
