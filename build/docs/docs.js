dbc4c013ffac7c3bb71f098ada4c575fdbbf9b32
MCJack123 <jackmacwindowslinux@gmail.com>
2021-04-15 23:13:16 -0400

    Added logo to docs header

--- build/docs/docs.js
+++ build/docs/docs.js
@@ -3,15 +3,17 @@ function toggleNav() {
     if (nav) {
         document.getElementById("docs-sidebar").className = "";
         document.getElementById("docs-content").className = "";
-        document.getElementsByClassName("docs-header")[0].style.position = "sticky";
+        document.getElementById("docs-content").onclick = null;
+        document.getElementsByClassName("docs-header")[0].style.position = null;
         document.getElementsByClassName("docs-box")[0].style.marginTop = null;
         const scrollY = document.body.style.top;
-        document.body.style.position = '';
-        document.body.style.top = '';
+        document.body.style.position = null;
+        document.body.style.top = null;
         window.scrollTo(0, parseInt(scrollY || '0') * -1);
     } else {
         document.getElementById("docs-sidebar").className = "open";
         document.getElementById("docs-content").className = "gray";
+        document.getElementById("docs-content").onclick = toggleNav;
         document.getElementsByClassName("docs-header")[0].style.position = "fixed";
         document.getElementsByClassName("docs-box")[0].style.marginTop = "50px";
         document.body.style.top = `-${window.scrollY}px`;
