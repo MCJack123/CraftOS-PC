b8e28543d6aaedef5f080ead256fc7d54076a296
JackMacWindows <jackmacwindowslinux@gmail.com>
2021-10-17 19:18:04 -0400

    Moved docs & images building to parallel

--- build.js
+++ build.js
@@ -31,27 +31,27 @@ fs.readdir("build/images").then(imageFiles => {
             console.error(err);
             process.exit(1);
         });
+}).catch(err => {
+    console.error(err);
+    process.exit(1);
+});
 
-    Promise.all([
-        fs.readFile("docs/sidebar.md", {encoding: "utf8"}),
-        fs.readdir("docs")
-    ]).then(res => {
-        let sidebar = marked(res[0]);
-        for (let file of res[1]) {
-            if (file !== "sidebar.md") {
-                fs.readFile("docs/" + file, {encoding: "utf8"})
-                    .then(data => ejs.renderFile("src/docs.ejs", {info: docsInfo[file.match(/[^%.]+/)], sidebar: sidebar, content: marked(data)}))
-                    .then(data => fs.writeFile("build/docs/" + file.replace(".md", ".html"), data, {encoding: "utf8"}))
-                    .catch(err => {
-                        console.error(err);
-                        process.exit(1);
-                    });
-                }
+Promise.all([
+    fs.readFile("docs/sidebar.md", {encoding: "utf8"}),
+    fs.readdir("docs")
+]).then(res => {
+    let sidebar = marked(res[0]);
+    for (let file of res[1]) {
+        if (file !== "sidebar.md") {
+            fs.readFile("docs/" + file, {encoding: "utf8"})
+                .then(data => ejs.renderFile("src/docs.ejs", {info: docsInfo[file.match(/[^%.]+/)], sidebar: sidebar, content: marked(data)}))
+                .then(data => fs.writeFile("build/docs/" + file.replace(".md", ".html"), data, {encoding: "utf8"}))
+                .catch(err => {
+                    console.error(err);
+                    process.exit(1);
+                });
         }
-    }).catch(err => {
-        console.error(err);
-        process.exit(1);
-    });
+    }
 }).catch(err => {
     console.error(err);
     process.exit(1);
